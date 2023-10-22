3bf6b25e395e576ee8dd97d7e436a98d9de52405
Abylai <221714@astanait.edu.kz>
2023-10-22 23:44:30 +0200

    Cтарая и новая база данных

---
+++ src/OldDatabase.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+class OldDatabase {
+    void fetchPatientData(String patientId){
+        System.out.println("Получение данных о пациенте из старой базы данных: " + patientId);
+    }
+}
