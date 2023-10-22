f2cff3ceedcd9e6fc8e0d85c14c257e7d44c9135
Abylai <221714@astanait.edu.kz>
2023-10-22 23:59:40 +0200

    Мэйн

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import javax.xml.crypto.Data;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        NewDatabase newDB = new NewDatabase();
+        DatabaseAdapter databaseAdapter = new DatabaseAdapter(newDB);
+        databaseAdapter.fetchPatientData("12345");
+        newDB.retrievePatientInfo("67890");
+
     }
 }
